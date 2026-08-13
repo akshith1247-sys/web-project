38a1728584ff0403a334e653ef65110db6ab9ae4
AK <akshith1247@gmail.com>
2026-08-13 21:20:15 +0530

    readME changed

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,26 @@
-alert("hello from javascript")
+//alert("hello from javascript")
+const users=[
+    {
+        "name":"John Doe",
+        "gender":"Male",
+        "image": "john.png",
+
+    },
+    {
+        "name":"Jane Doe",
+        "gender":"Female",
+        "image": "jane.png",
+    }
+]
+
+var currentIndex=0;
+
+function toggle(){
+    if(currentIndex == 0)
+        currentIndex = 1;
+    else
+        currentIndex = 0;
+    document.getElementById("card-image").src = users[currentIndex].image;
+    document.getElementById("card-name").innerText = users[currentIndex].name;
+    document.getElementById("card-gender").innerText = users[currentIndex].gender;
+}
