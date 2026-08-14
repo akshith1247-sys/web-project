f8371105b3ad122ba6c0d5f2632e4955a4b5a04c
AK <akshith1247@gmail.com>
2026-08-14 11:10:31 +0530

    readme changed

--- frontend/index.js
+++ frontend/index.js
@@ -24,3 +24,20 @@ function toggle(){
     document.getElementById("card-name").innerText = users[currentIndex].name;
     document.getElementById("card-gender").innerText = users[currentIndex].gender;
 }
+
+function random(){
+
+fetch("https://randomuser.me/api")
+    .then(function(response){
+        return response.json();
+})
+.then(function(data){
+    var details = data.results[0];
+    document.getElementById("card-image").src = details.picture.large;
+    var fullname = details.name.title + " " + details.name.first + " " + details.name.last;
+    document.getElementById("card-name").innerText = fullname;
+    document.getElementById("card-gender").innerText = details.gender;
+
+})
+
+}
